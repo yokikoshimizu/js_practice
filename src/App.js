af0225e7690f11ce35dc165877e3bf0c31eccfda
yokikoshimizu <heppoko.yokiki@gmail.com>
2022-02-11 02:27:18 +0900

    .

---
+++ src/App.js
@@ -0,0 +1,6 @@
+const question = '次のうち、関西地方ではない都道府県はどれ？';
+const answers = ['大阪府', '京都府', '兵庫県', '滋賀県'];
+const corrent = '滋賀県';
+
+console.log('')
+document.getElementById('question').textContent = question
